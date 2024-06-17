5dc10b77bd8e735785bf4151f71b1a6d25647da0
Subaruuwu <fylhtq2004.1yande@yandex.ru>
2024-06-17 08:57:04 +0500

    a little refactoring has been done

--- app/buttons.py
+++ app/buttons.py
@@ -12,11 +12,5 @@ def make_button(window, text, command, row, column, x, y, state='normal'):
 def make_combobox(dict_of_tasks: dict, window: tk, row: int, column: int, x: int, y: int):
     combobox = ttk.Combobox(window, values=[task for task in dict_of_tasks.values()])
     combobox.grid(row=row, column=column, padx=x, pady=y)
-    combobox.bind("<<ComboboxSelected>>")  # , lambda event: on_combobox_select(event, combobox, description_label)
+    combobox.bind("<<ComboboxSelected>>")
     return combobox
-
-
-# def on_combobox_select(event):
-#     selected_item = combobox.get()
-#     description_label.config(text=f"Выбран вариант: {dict_of_description[selected_item]}")
-#     #Добавить появление примера в поле для вывода с помощью словаря и атрибут input_data
